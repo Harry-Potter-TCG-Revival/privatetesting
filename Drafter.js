ddf735ded3442a42f1bc159ba04ad741c0d27889
geldwin <63691740+geldwin@users.noreply.github.com>
2025-01-31 21:25:23 +0800

    Update Drafter.js

--- Drafter.js
+++ Drafter.js
@@ -30,7 +30,7 @@ const cardList = [
     { name: "Weasley Ghoul", rarity: "U", isHorizontal: true, imageFile: "WeasleyGhoul.png", cost: 6, type: "F" , draftValue: 5, setName: "The World Cup"},
     { name: "Anti Muggle Security Clean-up", rarity: "C", isHorizontal: false, imageFile: "AntiMuggleSecurityCleanUp.png", cost: 6, type: "C", draftValue: 5 , setName: "The World Cup"},
     { name: "Bagman's Gamble", rarity: "C", isHorizontal: false, imageFile: "BagmansGamble.png", cost: 9, type: "C" , draftValue: 5, setName: "The World Cup"},
-    { name: "Box of Portkeys", rarity: "U", isHorizontal: true, imageFile: "BoxOfPortkeys.png", cost: 5, type: "C", draftValue: 5 , setName: "The World Cup"},
+    { name: "Box of Portkeys", rarity: "U", isHorizontal: false, imageFile: "BoxOfPortkeys.png", cost: 5, type: "C", draftValue: 5 , setName: "The World Cup"},
     { name: "Gifts of Gold", rarity: "C", isHorizontal: false, imageFile: "GiftsOfGold.png", cost: 4, type: "C", draftValue: 5, setName: "The World Cup" },
     { name: "Lantern Lit Trail", rarity: "R", isHorizontal: true, imageFile: "LanternLitTrail.png", cost: 5, type: "C" , draftValue: 5, setName: "The World Cup"},
     { name: "Meeting of the Ministers", rarity: "C", isHorizontal: true, imageFile: "MeetingOfTheMinisters.png", cost: 6, type: "C", draftValue: 5 , setName: "The World Cup"},
