ae1004a83e75d48c3f7aedee867342dc064d778e
geldwin <63691740+geldwin@users.noreply.github.com>
2025-02-01 18:31:41 +0800

    Update Drafter.js

--- Drafter.js
+++ Drafter.js
@@ -27,7 +27,7 @@ const cardList = [
     { name: "Veela", rarity: "C", isHorizontal: true, imageFile: "Veela.png", cost: 7, type: "F", draftValue: 5, setName: "The World Cup" },
     { name: "Veela Dance", rarity: "C", isHorizontal: false, imageFile: "VeelaDance.png", cost: 5, type: "F" , draftValue: 5, setName: "The World Cup"},
     { name: "Weasley Ghoul", rarity: "U", isHorizontal: true, imageFile: "WeasleyGhoul.png", cost: 6, type: "F" , draftValue: 5, setName: "The World Cup"},
-    { name: "Anti Muggle Security Clean-up", rarity: "C", isHorizontal: false, imageFile: "AntiMuggleSecurityCleanup.png", cost: 6, type: "C", draftValue: 5 , setName: "The World Cup"},
+    { name: "Anti Muggle Security Clean-up", rarity: "C", isHorizontal: false, imageFile: "AntiMuggleSecurityCleanUp.png", cost: 6, type: "C", draftValue: 5 , setName: "The World Cup"},
     { name: "Bagman's Gamble", rarity: "C", isHorizontal: false, imageFile: "BagmansGamble.png", cost: 9, type: "C" , draftValue: 5, setName: "The World Cup"},
     { name: "Box of Portkeys", rarity: "U", isHorizontal: true, imageFile: "BoxOfPortkeys.png", cost: 5, type: "C", draftValue: 5 , setName: "The World Cup"},
     { name: "Gifts of Gold", rarity: "C", isHorizontal: false, imageFile: "GiftsOfGold.png", cost: 4, type: "C", draftValue: 5, setName: "The World Cup" },
